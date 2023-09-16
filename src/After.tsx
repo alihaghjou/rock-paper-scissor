a8f1a39768f389469074f0016311dd3454990b84
alihaghjou <alihaghjou30@gmail.com>
2023-09-16 16:20:27 +0330

    some minor styling

--- src/After.tsx
+++ src/After.tsx
@@ -36,20 +36,20 @@ export default function After({
   const userImage = arr.find((item) => item.name === user)?.image;
   const houseImage = arr.find((item) => item.name === house)?.image;
   return (
-    <article className="flex justify-between">
-      <div className="flex flex-col">
+    <article className="flex justify-between items-center">
+      <div className="flex flex-col items-center justify-center">
         <h2 className="uppercase">You picked</h2>
-        <img src={userImage} />
+        <img src={userImage} className="w-24"/>
       </div>{" "}
-      <div>
-        <div>{result}</div>
-        <button disabled={disable} onClick={() => setPlayState(true)}>
+      <div className="flex flex-col">
+        <div className="text-center uppercase text-3xl">You {result}</div>
+        <button className="bg-white text-black rounded py-1" disabled={disable} onClick={() => setPlayState(true)}>
           Play Again
         </button>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-center">
         <h2 className="uppercase">the House picked</h2>
-        <img src={houseImage} />
+        <img className="w-24" src={houseImage} />
       </div>
     </article>
   );
