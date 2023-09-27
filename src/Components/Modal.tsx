6f198a6b7230a65acaa1eb90c9a12b2806d93939
alihaghjou <alihaghjou30@gmail.com>
2023-09-27 23:43:21 +0330

    mobile view for rules

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -24,7 +24,7 @@ export default function MyModal() {
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-10 " onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -37,7 +37,7 @@ export default function MyModal() {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 overflow-y-auto ">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
@@ -48,10 +48,10 @@ export default function MyModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="md:max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all md:w-full md:h-full w-screen h-screen">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900 flex justify-between items-center"
+                    className="text-3xl font-medium leading-6 text-gray-900 flex justify-between items-center"
                   >
                     <span>RULES</span>
                     <div>
@@ -60,9 +60,9 @@ export default function MyModal() {
                       </button>
                     </div>
                   </Dialog.Title>
-                  <div className="mt-2">
+                  <div className="md:mt-10 flex justify-center items-center h-full">
                     <center>
-                      <img src={Rules} />
+                      <img src={Rules} className="w-96" />
                     </center>
                   </div>
                 </Dialog.Panel>
